010953be1f374d7c98d7808d597e8b41a67b85db
sumon <18103159@iubat.edu>
2023-01-25 17:57:31 +0600

    tailwind install

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>iStore</title>
-    {{-- @vite('resources/css/app.css')
-    @notifyCss --}}
+    @vite('resources/css/app.css')
+    {{-- @notifyCss --}}
     <link href="{{ asset('backend/master/css/googleapi.css') }}" rel="stylesheet"/>
     <link rel="stylesheet" href="{{ asset('backend/master/css/tailwind.output.css') }}"/>
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.6.0/dist/flowbite.min.css" />
