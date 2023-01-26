727eae4d31aa734f742e24edeb384bd34102b598
sumon <18103159@iubat.edu>
2023-01-26 15:18:57 +0600

    profile with image

--- resources/views/backend/pages/user/profile.blade.php
+++ resources/views/backend/pages/user/profile.blade.php
@@ -3,10 +3,10 @@
 <body class=" font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover">
 	<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
 		<!--Main Col-->
-		<div id="profile" class="w-full mt-40 lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+		<div id="profile" class="w-full mt-28 lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
 			<div class="p-4 md:p-12 text-center lg:text-left">
 				<!-- Image for mobile view-->
-				<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('{{ url('/uploads/',$profile->image) }}')"></div>
+				<div class="block rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('{{ url('/uploads',$profile->image) }}')"></div>
 				<h1 class="text-3xl font-bold pt-8 lg:pt-0">{{ $profile->name }}</h1>
 
 				<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
